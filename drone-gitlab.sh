324cb7fba58044d4e0dd44ddd30a8e10957b6f00
Glux <gluxhappy@gmail.com>
2022-05-30 14:36:58 +0800

    fix scripts and update readme

--- drone-gitlab.sh
+++ drone-gitlab.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 go_scm_version=v1.21.1
 drone_server_version=v2.12.1
 
@@ -21,22 +25,22 @@ go_docker_base="$go_docker_base golang:1.14.15"
 if [ -d "go-scm" ]; then
    echo "Found existing code base for go-scm"
 else 
-    git clone git@github.com:drone/go-scm.git || exit 1
+    git clone git@github.com:drone/go-scm.git
     cd go-scm
-    git fetch --tags || exit 1
-    git checkout -b ${go_scm_version} ${go_scm_version} || exit 1
-    git apply ../go-scm.patch || exit 1
+    git fetch --tags
+    git checkout -b ${go_scm_version} ${go_scm_version}
+    git apply ../go-scm.patch
 fi
 
 cd "$BASE_DIR"
 if [ -d "drone" ]; then
    echo "Found existing code base for drone-server"
 else
-    git clone git@github.com:harness/drone.git || exit 1
-    cd drone || exit 1
-    git fetch --tags || exit 1
-    git checkout -b ${drone_server_version} ${drone_server_version} || exit 1   
-    git apply ../drone.patch || exit 1
+    git clone git@github.com:harness/drone.git
+    cd drone
+    git fetch --tags
+    git checkout -b ${drone_server_version} ${drone_server_version}
+    git apply ../drone.patch
     ${go_docker_base} go mod edit -replace=github.com/drone/go-scm=../go-scm
     # curl -L -o drone.zip https://github.com/harness/drone/archive/refs/tags/v2.12.0.zip
     # unzip drone.zip
@@ -44,16 +48,20 @@ fi
 
 ${go_docker_base} /usr/bin/env bash scripts/build.sh
 
-cd drone
+cd "$BASE_DIR"/drone
 
 timestamp=$(date +%Y%m%d%H%M%S)
 docker_image_version="${drone_server_version}_${timestamp}"
 docker_image_tag="drone-server-gitlab:${docker_image_version}"
-ls -al release/linux/amd64
 docker build -t ${docker_image_tag} -f docker/Dockerfile.server.linux.amd64 .
 
 # comment out if you want to push to dockerhub
-exit 0
+# exit 0
+
+if [ -f "$BASE_DIR/ecr.sh" ]; then
+    bash "$BASE_DIR/ecr.sh" "$docker_image_tag"
+fi
+
 push_tag="gluxhappy/${docker_image_tag}"
 docker tag ${docker_image_tag} ${push_tag}
 docker push $push_tag
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 1. run the shell script ```bash drone-gitlab.sh``` to build a image with gitlab token refresh fix.
 2. update the ```drone.patch``` or the ```go-scm.patch``` if you have additional changes.
 3. A prebuilt image is already at [gluxhappy/drone-server-gitlab](https://hub.docker.com/repository/docker/gluxhappy/drone-server-gitlab)
+
+
+### Attention
+
+Please use [DRONE_GIT_USERNAME](https://docs.drone.io/server/reference/drone-git-username/) and [DRONE_GIT_PASSWORD](https://docs.drone.io/server/reference/drone-git-password/) to avoid a token refresh issue casued by [Force Token Refresh](https://github.com/harness/drone/blob/v2.12.1/service/netrc/netrc.go#L74).  
+
+The ```DRONE_GIT_USERNAME``` should be a user's name and ```DRONE_GIT_PASSWORD``` should be an [Personal Access Token](https://gitlab.com/-/profile/personal_access_tokens) with long expiration time. The access token should has been grant at least read access to all repositories you want to build via the Drone.
